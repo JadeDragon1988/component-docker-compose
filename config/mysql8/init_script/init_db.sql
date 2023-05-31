4790cf252b19f21605e8ff7f73e991eefbd84c99
Jade <wangyulong1@yonyou.com>
2023-05-31 15:14:10 +0800

    update

--- config/mysql8/init_script/init_db.sql
+++ config/mysql8/init_script/init_db.sql
@@ -1,9 +1,11 @@
 # SET GLOBAL SQL_MODE = 'STRICT_TRANS_TABLES,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
-CREATE USER 'porta' IDENTIFIED BY 'porta';
+select * from mysql.user;
+
+CREATE USER 'porta'@'%' IDENTIFIED BY 'porta';
 GRANT ALL PRIVILEGES ON *.* TO 'porta'@'%';
 
-CREATE DATABASE portal_site CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+CREATE DATABASE if not exists portal_site CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
 
-
+flush privileges;
 commit;
